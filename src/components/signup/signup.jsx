7bac855f48e96bd8e3016753a6964572a944e894
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-03 21:38:28 +0200

    Created the signup page

---
+++ src/components/signup/signup.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../login/login.css";
+
+const SignupPage = () => {
+  return (
+    <div className="auth-container">
+      <div className="welcome-text">Welcome to Shamun Co.</div>
+      <div className="auth-form">
+        <h2>Sign Up</h2>
+        <form>
+          <input type="text" placeholder="Username" required />
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
