1416acad263e1c22cdd0edf9affdf2601d639a5b
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-04 21:44:48 +0200

    Fixed image path

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import './dashboard.css'; 
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./dashboard.css";
+import { Link } from "react-router-dom";
 
 const sections = [
-  { name: 'POS System', image: '/path/to/pos-system-image.jpg' },
-  { name: 'Invoices', image: '/path/to/invoices-image.jpg' },
-  { name: 'Inventory', image: '/path/to/inventory-image.jpg' },
-  { name: 'Customers', image: '/path/to/customers-image.jpg' },
-  { name: 'Settings', image: '/path/to/settings-image.jpg' },
+  { name: "POS System", image: "../../../assests/accounting.jpeg" },
+  { name: "Invoices", image: "/path/to/invoices-image.jpg" },
+  { name: "Inventory", image: "/path/to/inventory-image.jpg" },
+  { name: "Customers", image: "/path/to/customers-image.jpg" },
+  { name: "Settings", image: "/path/to/settings-image.jpg" },
 ];
-
-
-
 
 const DashboardPage = () => {
   return (
@@ -19,9 +16,17 @@ const DashboardPage = () => {
       <h1>Dashboard</h1>
       <div className="card-grid">
         {sections.map((section, index) => (
-          <Link to={`/${section.name.toLowerCase().replace(' ', '-')}`} key={index} className="dashboard-card-link">
+          <Link
+            to={`/${section.name.toLowerCase().replace(" ", "-")}`}
+            key={index}
+            className="dashboard-card-link"
+          >
             <div className="dashboard-card">
-              <img src={section.image} alt={section.name} className="card-image" />
+              <img
+                src={section.image}
+                alt={section.name}
+                className="card-image"
+              />
               <div className="card-title">{section.name}</div>
             </div>
           </Link>
@@ -30,6 +35,5 @@ const DashboardPage = () => {
     </div>
   );
 };
-
 
 export default DashboardPage;
