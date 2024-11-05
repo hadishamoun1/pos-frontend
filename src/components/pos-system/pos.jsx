eaec9c794ed68fd6d9e5651dc9f9aef44b03d6e5
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-05 21:06:00 +0200

    Created the fisrt 3 container

---
+++ src/components/pos-system/pos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './posSystem.css';
+
+const POSSystemPage = () => {
+  return (
+    <div className="pos-container">
+      <div className="pos-left">Left Container</div>
+      <div className="pos-center">Center Container</div>
+      <div className="pos-right">Right Container</div>
+    </div>
+  );
+};
+
+export default POSSystemPage;
