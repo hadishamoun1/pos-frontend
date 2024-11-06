9b45b04a8a0a3556a578e2024561cfc215f6f8e8
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-06 22:27:36 +0200

    Created two container

---
+++ src/components/accounting/accounting.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./accounting.css";
+
+const AccountingPage = () => {
+  return (
+    <div className="accounting-container">
+      <div className="accounting-section top-section">
+        {/* Content for the top section */}
+        <h2>Top Accounting Section</h2>
+      </div>
+      <div className="accounting-section bottom-section">
+        {/* Content for the bottom section */}
+        <h2>Bottom Accounting Section</h2>
+      </div>
+    </div>
+  );
+};
+
+export default AccountingPage;
