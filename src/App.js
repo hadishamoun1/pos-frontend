6510cc22dd85e7d83bd773dd7221ba0c040d138c
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-06 22:29:10 +0200

    Added path for accounting page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LoginPage from "./components/login/login";
 import SignupPage from "./components/signup/signup";
 import DashboardPage from "./components/dashboard/dashboard";
 import POSSystemPage from "./components/pos-system/pos";
+import AccountingPage from "./components/accounting/accounting";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/Signup" element={<SignupPage />} />
         <Route path="/Dashboard" element={<DashboardPage />} />
         <Route path="/pos-system" element={<POSSystemPage />} />
+        <Route path="/accounting" element={<AccountingPage />} />
+
       </Routes>
     </Router>
   );
