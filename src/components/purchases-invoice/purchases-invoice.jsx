4359cd23bd7ce0ecc7ade8adc6441750bbe0bc74
hadishamoun1 <shamounhadi2@gmail.com>
2024-11-15 21:56:22 +0200

    Changed select items popup to scrollable

--- src/components/purchases-invoice/purchases-invoice.jsx
+++ src/components/purchases-invoice/purchases-invoice.jsx
@@ -261,47 +261,51 @@ const PurchasesInvoicePage = () => {
               onChange={(e) => setSearchQuery(e.target.value)}
               className="modal-search"
             />
-            <table className="modal-items-table">
-              <thead>
-                <tr>
-                  <th>Select</th>
-                  <th>Item Name</th>
-                  <th>Type</th>
-                  <th>Origin</th>
-                  <th>Length (cm)</th>
-                  <th>Width (cm)</th>
-                  <th>Sheets/Box</th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredItems.map((item) =>
-                  item.dimensions.map((dimension) => (
-                    <tr key={`${item.id}-${dimension.dimensionId}`}>
-                      <td>
-                        <input
-                          type="checkbox"
-                          onChange={() => handleCheckboxChange(item, dimension)}
-                          checked={selectedItems.some(
-                            (selectedItem) =>
-                              selectedItem.itemName === item.itemName &&
-                              selectedItem.origin === dimension.origin &&
-                              selectedItem.length === dimension.length &&
-                              selectedItem.width === dimension.width &&
-                              selectedItem.type === item.type
-                          )}
-                        />
-                      </td>
-                      <td>{item.itemName}</td>
-                      <td>{item.type}</td>
-                      <td>{dimension.origin}</td>
-                      <td>{dimension.length}</td>
-                      <td>{dimension.width}</td>
-                      <td>{dimension.sheetsPerBox || ""}</td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
+            <div className="modal-items-table-container">
+              <table className="modal-items-table">
+                <thead>
+                  <tr>
+                    <th>Select</th>
+                    <th>Item Name</th>
+                    <th>Type</th>
+                    <th>Origin</th>
+                    <th>Length (cm)</th>
+                    <th>Width (cm)</th>
+                    <th>Sheets/Box</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredItems.map((item) =>
+                    item.dimensions.map((dimension) => (
+                      <tr key={`${item.id}-${dimension.dimensionId}`}>
+                        <td>
+                          <input
+                            type="checkbox"
+                            onChange={() =>
+                              handleCheckboxChange(item, dimension)
+                            }
+                            checked={selectedItems.some(
+                              (selectedItem) =>
+                                selectedItem.itemName === item.itemName &&
+                                selectedItem.origin === dimension.origin &&
+                                selectedItem.length === dimension.length &&
+                                selectedItem.width === dimension.width &&
+                                selectedItem.type === item.type
+                            )}
+                          />
+                        </td>
+                        <td>{item.itemName}</td>
+                        <td>{item.type}</td>
+                        <td>{dimension.origin}</td>
+                        <td>{dimension.length}</td>
+                        <td>{dimension.width}</td>
+                        <td>{dimension.sheetsPerBox || ""}</td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+            </div>
             <button className="close-modal-button" onClick={closeItemModal}>
               Close
             </button>
