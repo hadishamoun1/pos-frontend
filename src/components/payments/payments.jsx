4f9e2f350ff9973d30bef80044c6f45bd131b6c6
hadishamoun1 <shamounhadi2@gmail.com>
2025-01-05 02:20:39 +0200

    Added class name for each column and fixed there width

--- src/components/payments/payments.jsx
+++ src/components/payments/payments.jsx
@@ -14,9 +14,9 @@ const PaymentsPage = () => {
       checkNumber: "123456",
       bankName: "Bank A",
       dueDate: "2025-01-15",
-      receiptNumber: "R001",
+      paymentNumber: "PM001",
       comments: "Payment for order 123",
-      receiptType: "S",
+      paymentType: "S",
       dateCreated: "2025-01-01",
       dateModified: "2025-01-02",
       doneBy: "Admin",
@@ -41,18 +41,18 @@ const PaymentsPage = () => {
             <th className="payment-voucher-amount">Amount</th>
             <th className="payment-voucher-currency">Currency</th>
             <th className="payment-voucher-date">Date</th>
-            <th>Type</th>
-            <th>Exchange Rate</th>
-            <th>Amount Exchanged</th>
-            <th>Check Number</th>
-            <th>Bank Name</th>
-            <th>Due Date</th>
-            <th>Receipt Number</th>
-            <th className="payment-voucher-comments">Comments</th>
-            <th>Receipt Type</th>
-            <th>Date Created</th>
-            <th>Date Modified</th>
-            <th>Done By</th>
+            <th className="payment-voucher-type">Type</th>
+            <th className="payment-voucher-exchange-rate">Ex Rate</th>
+            <th className="payment-voucher-amount-exchanged">Amount Ex</th>
+            <th className="payment-voucher-check-number">Check #</th>
+            <th className="payment-voucher-bank-name">Bank Name</th>
+            <th className="payment-voucher-due-date">Due Date</th>
+            <th className="payment-voucher-payment-number">Payment #</th>
+            <th className="payment-voucher-comments">Comment</th>
+            <th className="payment-voucher-payment-type">Pmt Type</th>
+            <th className="payment-voucher-date-created">Date Created</th>
+            <th className="payment-voucher-date-modified">Date Modified</th>
+            <th className="payment-voucher-done-by">Done By</th>
           </tr>
         </thead>
         <tbody>
@@ -62,18 +62,32 @@ const PaymentsPage = () => {
               <td className="payment-voucher-amount">{row.amount}</td>
               <td className="payment-voucher-currency">{row.currency}</td>
               <td className="payment-voucher-date">{row.date}</td>
-              <td>{row.type}</td>
-              <td>{row.exchangeRate}</td>
-              <td>{row.amountExchanged}</td>
-              <td>{row.checkNumber}</td>
-              <td>{row.bankName}</td>
-              <td>{row.dueDate}</td>
-              <td>{row.receiptNumber}</td>
+              <td className="payment-voucher-type">{row.type}</td>
+              <td className="payment-voucher-exchange-rate">
+                {row.exchangeRate}
+              </td>
+              <td className="payment-voucher-amount-exchanged">
+                {row.amountExchanged}
+              </td>
+              <td className="payment-voucher-check-number">
+                {row.checkNumber}
+              </td>
+              <td className="payment-voucher-bank-name">{row.bankName}</td>
+              <td className="payment-voucher-due-date">{row.dueDate}</td>
+              <td className="payment-voucher-payment-number">
+                {row.paymentNumber}
+              </td>
               <td className="payment-voucher-comments">{row.comments}</td>
-              <td>{row.receiptType}</td>
-              <td>{row.dateCreated}</td>
-              <td>{row.dateModified}</td>
-              <td>{row.doneBy}</td>
+              <td className="payment-voucher-payment-type">
+                {row.paymentType}
+              </td>
+              <td className="payment-voucher-date-created">
+                {row.dateCreated}
+              </td>
+              <td className="payment-voucher-date-modified">
+                {row.dateModified}
+              </td>
+              <td className="payment-voucher-done-by">{row.doneBy}</td>
             </tr>
           ))}
         </tbody>
