77836f5cc4df9c60fbbda7e449c654013c27f564
hadishamoun1 <shamounhadi2@gmail.com>
2025-02-11 15:44:38 +0200

    Changed table to scrollable and added styles for scrolling

--- src/components/pos-system/pos.jsx
+++ src/components/pos-system/pos.jsx
@@ -205,85 +205,88 @@ const POSSystemPage = () => {
             </div>
           </div>
         </div>
-
-        {/* Inventory Table */}
-        <table className="pos-page-inventory-table">
-          <thead>
-            <tr>
-              <th>Origin</th>
-              <th>Item</th>
-              <th>Type</th>
-              <th>Length</th>
-              <th>Width</th>
-              <th>Box</th>
-              <th>Sheet</th>
-              <th>SQM</th>
-              <th>Price</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map((row, index) => (
-              <tr
-                key={index}
-                onClick={() => handleRowClick(index)}
-                onContextMenu={(e) => handleRightClick(e, index)}
-                className={index === selectedRowIndex ? "pos-selected-row" : ""}
-              >
-                <td>
-                  <input type="text" value={row.origin} readOnly />
-                </td>
-                <td>
-                  <input type="text" value={row.item} readOnly />
-                </td>
-                <td>
-                  <input type="text" value={row.type} readOnly />
-                </td>
-                <td>
-                  <input type="text" value={row.length} readOnly />
-                </td>
-                <td>
-                  <input type="text" value={row.width} readOnly />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    value={row.box}
-                    onChange={(e) =>
-                      handleInputChange(index, "box", e.target.value)
-                    }
-                    disabled={row.type === "sheet"}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    value={row.sheet}
-                    onChange={(e) =>
-                      handleInputChange(index, "sheet", e.target.value)
-                    }
-                    readOnly={row.type === "box"}
-                  />
-                </td>
-                <td>
-                  <input type="text" value={row.sqm} readOnly />
-                </td>
-                <td>
-                  <input
-                    type="number"
-                    value={row.price}
-                    onChange={(e) =>
-                      handleInputChange(index, "price", e.target.value)
-                    }
-                  />
-                </td>
-                <td>
-                  <input type="text" value={row.total} readOnly />
-                </td>{" "}
+        <div className="pos-page-inventory-table-container">
+          {/* Inventory Table */}
+          <table className="pos-page-inventory-table">
+            <thead>
+              <tr>
+                <th>Origin</th>
+                <th>Item</th>
+                <th>Type</th>
+                <th>Length</th>
+                <th>Width</th>
+                <th>Box</th>
+                <th>Sheet</th>
+                <th>SQM</th>
+                <th>Price</th>
+                <th>Total</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {tableData.map((row, index) => (
+                <tr
+                  key={index}
+                  onClick={() => handleRowClick(index)}
+                  onContextMenu={(e) => handleRightClick(e, index)}
+                  className={
+                    index === selectedRowIndex ? "pos-selected-row" : ""
+                  }
+                >
+                  <td>
+                    <input type="text" value={row.origin} readOnly />
+                  </td>
+                  <td>
+                    <input type="text" value={row.item} readOnly />
+                  </td>
+                  <td>
+                    <input type="text" value={row.type} readOnly />
+                  </td>
+                  <td>
+                    <input type="text" value={row.length} readOnly />
+                  </td>
+                  <td>
+                    <input type="text" value={row.width} readOnly />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      value={row.box}
+                      onChange={(e) =>
+                        handleInputChange(index, "box", e.target.value)
+                      }
+                      disabled={row.type === "sheet"}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      value={row.sheet}
+                      onChange={(e) =>
+                        handleInputChange(index, "sheet", e.target.value)
+                      }
+                      readOnly={row.type === "box"}
+                    />
+                  </td>
+                  <td>
+                    <input type="text" value={row.sqm} readOnly />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      value={row.price}
+                      onChange={(e) =>
+                        handleInputChange(index, "price", e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input type="text" value={row.total} readOnly />
+                  </td>{" "}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
 
       {/* Right Sidebar */}
