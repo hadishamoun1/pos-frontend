cc776f422b0d02fbd5143d04d07ae57d7d5f450b
Hadi Shamoun <shamounhadi1@gmail.com>
2026-01-02 17:29:07 +0200

    Created export button

--- src/components/vouchers/Exportjvtoexcel.jsx
+++
@@ -1,7 +0,0 @@
-// Add this export function to your JournalVoucherPage component
-// You'll need to install: npm install xlsx
-
-import * as XLSX from 'xlsx';
-
-// Add this function inside your JournalVoucherPage component, before the return statement
-
