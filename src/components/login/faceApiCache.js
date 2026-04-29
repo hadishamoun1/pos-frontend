8df58313c6128b1def73fe19d13b07a85cf9287a
Hadi Shamoun <shamounhadi1@gmail.com>
2026-04-29 03:10:27 +0300

    Fixed face cache

--- src/components/login/faceApiCache.js
+++ src/components/login/faceApiCache.js
@@ -1,18 +1,4 @@
 const FACE_MODELS_URL = "/models";
-const CACHE_NAME = "face-models-v1";
-
-async function cachedFetch(url, options) {
-  try {
-    const cache = await caches.open(CACHE_NAME);
-    const cached = await cache.match(url);
-    if (cached) return cached;
-    const res = await fetch(url, options);
-    if (res.ok) cache.put(url, res.clone());
-    return res;
-  } catch {
-    return fetch(url, options);
-  }
-}
 
 let faceapi = null;
 let modelsLoaded = false;
@@ -24,7 +10,6 @@ export async function preloadFaceModels() {
 
   loadingPromise = (async () => {
     if (!faceapi) faceapi = await import("face-api.js");
-    faceapi.env.monkeyPatch({ fetch: cachedFetch });
     await Promise.all([
       faceapi.nets.tinyFaceDetector.loadFromUri(FACE_MODELS_URL),
       faceapi.nets.faceLandmark68Net.loadFromUri(FACE_MODELS_URL),
